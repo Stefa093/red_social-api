a3e714246902751dabc9cda72a6d88fae289282b
Stefa093 <estefaniasalazar093@gmail.com>
2022-12-19 21:10:47 -0500

    recovery password and email

--- src/auth/auth.services.js
+++ src/auth/auth.services.js
@@ -44,10 +44,10 @@ const postRecoveryToken = (req, res) => {
         .then((data) => {
             if(data){
                 mailer.sendMail({
-                    from: '<test.academlo@gmail.com>',
+                    from: '<estefaniasalazar093@gmail.com>',
                     to: email,
                     subject: 'Recuperación de Contraseña',
-                    html: `<a href='${config.api.host}/api/v1/auth/recovery-password/${data.id}'>Recuperar contraseña</a>`
+                    html: `<a href='${config.api.host}/api/v1/auth/recovery-password/${data.id}'>${config.api.host}/api/v1/auth/recovery-password/${data.id}</a>`
                 })
             }
             res.status(200).json({message: 'Email sended!, Check your inbox'})
@@ -57,15 +57,25 @@ const postRecoveryToken = (req, res) => {
         })
 }
 
+const patchPassword = (req, res) => {
+    const id = req.params.id
+    const {password} = req.body
 
-
-
-
-
-
-
+    authControllers.changePassword(id, password)
+    .then(data => {
+        if (data) {
+            res.status(200).json({message: 'Password updated successfully'})
+        } else {
+            res.status(400).json({message: 'URL expired'})
+        }
+    })
+    .catch(err => {
+        res.status(400).json({message: err.message})
+    })
+}
 
 module.exports = {
     postLogin,
-    postRecoveryToken
+    postRecoveryToken,
+    patchPassword
 }
--- src/users/users.services.js
+++ src/users/users.services.js
@@ -44,7 +44,7 @@ const postUser = (req, res) => {
     userControllers.createUser({firstName, lastName, email, password,gender, birthday})
         .then(async(data) => {
             await mailer.sendMail({
-                from: '<test.academlo@gmail.com>',
+                from: '<estefaniasalazar093@gmail.com>',
                 to: data.email,
                 subject: `Bienvenido ${data.firstName}`,
                 html: `<h1>Bienvenido a nuestra app ${data.firstName}</h1> <a href="#" class="myButton">turquoise</a> `,
